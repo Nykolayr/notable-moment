aecc0791c9bf0083348241af11a483024845194a
nykolay <nykolayr@gmail.com>
2025-09-03 15:19:36 +0700

    new ios

---
+++ android/app/src/main/kotlin/com/notablemoments/notable_moments/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.notablemoments.notable_moments
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import Flutter
 import UIKit
-import YandexMapsMobile
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,8 +7,6 @@ import YandexMapsMobile
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    YMKMapKit.setLocale("ru_RU")
-    YMKMapKit.setApiKey("8be32f5b-3719-4f7e-b9a1-755305320727")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
